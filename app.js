20e3fb2c5da8313219b15776a543dbb5bdccfde7
slifszyc <sacha.lifszyc@gmail.com>
2015-06-02 14:07:31 -0300

    [topic] - Rename law to topic. Closes #35

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = require('./config');
 require('./source/events/signup')(notifier);
 require('./source/events/forgot-password')(notifier);
 require('./source/events/reply-argument')(notifier);
-require('./source/events/law-published')(notifier);
+require('./source/events/topic-published')(notifier);
 require('./source/events/topic-published-feed')(notifier);
 require('./source/events/topic-voted')(notifier);
 require('./source/events/topic-commented')(notifier);
